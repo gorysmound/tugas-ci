f3fa89cfc1ed69f02b502d7d58e7b73e71ccc0f4
gorysmound <paspur24@gmail.com>
2024-07-17 01:51:56 +0700

    menambahkan api data transaksi pembelian

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -102,38 +102,38 @@ class ApiController extends ResourceController
     }
 
     public function monthly()
-{
-    $data = [
-        'query' => [],
-        'results' => [],
-        'status' => ["code" => 401, "description" => "Unauthorized"]
-    ];
-
-    $headers = $this->request->headers();
-    $postData = $this->request->getPost();
-
-    $data['query'] = $postData;
-
-    array_walk($headers, function (&$value, $key) {
-        $value = $value->getValue();
-    });
-
-    if ($headers["Key"] == $this->apiKey) {
-        if ($postData['type'] == 'transaction') {
-            $result = $this->transaction->select('COUNT(*) as jml')->like('created_at', '' . $postData['tahun'] . '-' . $postData['bulan'] . '', 'after')->first();
-            $data['results'] = $result;
-            $data['status'] = ["code" => 200, "description" => "OK"];
-        } elseif ($postData['type'] == 'earning') {
-            $result = $this->transaction->select('sum(total_harga) as jml')->like('created_at', '' . $postData['tahun'] . '-' . $postData['bulan'] . '', 'after')->first();
-            $data['results'] = $result;
-            $data['status'] = ["code" => 200, "description" => "OK"];
-        } elseif ($postData['type'] == 'user') {
-            $result = $this->user->select('COUNT(*) as jml')->like('created_at', '' . $postData['tahun'] . '-' . $postData['bulan'] . '', 'after')->first();
-            $data['results'] = $result;
-            $data['status'] = ["code" => 200, "description" => "OK"];
+    {
+        $data = [
+            'query' => [],
+            'results' => [],
+            'status' => ["code" => 401, "description" => "Unauthorized"]
+        ];
+    
+        $headers = $this->request->headers();
+        $postData = $this->request->getPost();
+    
+        $data['query'] = $postData;
+    
+        array_walk($headers, function (&$value, $key) {
+            $value = $value->getValue();
+        });
+    
+        if ($headers["Key"] == $this->apiKey) {
+            if ($postData['type'] == 'transaction') {
+                $result = $this->transaction->select('COUNT(*) as jml')->like('created_at', '' . $postData['tahun'] . '-' . $postData['bulan'] . '', 'after')->first();
+                $data['results'] = $result;
+                $data['status'] = ["code" => 200, "description" => "OK"];
+            } elseif ($postData['type'] == 'earning') {
+                $result = $this->transaction->select('sum(total_harga) as jml')->like('created_at', '' . $postData['tahun'] . '-' . $postData['bulan'] . '', 'after')->first();
+                $data['results'] = $result;
+                $data['status'] = ["code" => 200, "description" => "OK"];
+            } elseif ($postData['type'] == 'user') {
+                $result = $this->user->select('COUNT(*) as jml')->like('created_at', '' . $postData['tahun'] . '-' . $postData['bulan'] . '', 'after')->first();
+                $data['results'] = $result;
+                $data['status'] = ["code" => 200, "description" => "OK"];
+            }
         }
+    
+        return $this->respond($data);
     }
-
-    return $this->respond($data);
-}
 }
